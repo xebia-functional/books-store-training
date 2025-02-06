4a9ba6bc923a2523778e2daa83e75de6e3984637
Flagn3 <flagn3@gmail.com>
2025-02-06 13:44:23 +0100

    refactor user class

---
+++ src/Main/user.java
@@ -0,0 +1,54 @@
+package Main;
+
+import java.util.Objects;
+
+public class user {
+
+    private String name;
+    private int id;
+
+
+    public user() {
+    }
+
+    public user(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "usuario{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        user usuario = (user) o;
+        return id == usuario.id && Objects.equals(name, usuario.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+}

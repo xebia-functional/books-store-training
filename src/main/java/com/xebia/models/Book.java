7479a5280c7b383c42ebac7b8ec46cee3ce67ab0
Flagn3 <flagn3@gmail.com>
2025-02-12 14:30:14 +0100

    updated class Book

--- src/main/java/com/xebia/models/Book.java
+++ src/main/java/com/xebia/models/Book.java
@@ -1,96 +1,42 @@
 package com.xebia.models;
 
-import java.time.LocalDate;
 import java.util.Objects;
 
 public class Book {
-  private String title;
-  private String author;
-  private String user;
-  private boolean available;
-  private LocalDate date;
 
-  public Book() {}
+    private String title;
+    private String author;
 
-  public Book(String title, String author) {
-    this.title = title;
-    this.author = author;
-    this.user = "";
-    this.available = true;
-  }
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
 
-  @Override
-  public String toString() {
-    return "Book{"
-        + "title='"
-        + title
-        + '\''
-        + ", author='"
-        + author
-        + '\''
-        + ", user='"
-        + user
-        + '\''
-        + ", available="
-        + available
-        + ", date="
-        + date
-        + '}';
-  }
+    public String getTitle() {
+        return title;
+    }
 
-  @Override
-  public boolean equals(Object o) {
-    if (o == null || getClass() != o.getClass()) return false;
-    Book book = (Book) o;
-    return isAvailable() == book.isAvailable()
-        && getDate() == book.getDate()
-        && Objects.equals(getTitle(), book.getTitle())
-        && Objects.equals(getAuthor(), book.getAuthor())
-        && Objects.equals(getUser(), book.getUser());
-  }
+    public String getAuthor() {
+        return author;
+    }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(getTitle(), getAuthor(), getUser(), isAvailable(), getDate());
-  }
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
 
-  public String getTitle() {
-    return title;
-  }
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    }
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getAuthor() {
-    return author;
-  }
-
-  public void setAuthor(String author) {
-    this.author = author;
-  }
-
-  public String getUser() {
-    return user;
-  }
-
-  public void setUser(String user) {
-    this.user = user;
-  }
-
-  public boolean isAvailable() {
-    return available;
-  }
-
-  public void setAvailable(boolean available) {
-    this.available = available;
-  }
-
-  public LocalDate getDate() {
-    return date;
-  }
-
-  public void setDate(LocalDate date) {
-    this.date = date;
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author);
+    }
 }

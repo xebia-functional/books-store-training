c685b798eb1c0c4c44d744457d3125a05cec0842
Flagn3 <flagn3@gmail.com>
2025-02-12 14:34:53 +0100

    New Book model

--- src/main/java/com/xebia/models/Book.java
+++ src/main/java/com/xebia/models/Book.java
@@ -4,39 +4,36 @@ import java.util.Objects;
 
 public class Book {
 
-    private String title;
-    private String author;
+  private String title;
+  private String author;
 
-    public Book(String title, String author) {
-        this.title = title;
-        this.author = author;
-    }
+  public Book(String title, String author) {
+    this.title = title;
+    this.author = author;
+  }
 
-    public String getTitle() {
-        return title;
-    }
+  public String getTitle() {
+    return title;
+  }
 
-    public String getAuthor() {
-        return author;
-    }
+  public String getAuthor() {
+    return author;
+  }
 
-    @Override
-    public String toString() {
-        return "Book{" +
-                "title='" + title + '\'' +
-                ", author='" + author + '\'' +
-                '}';
-    }
+  @Override
+  public String toString() {
+    return "Book{" + "title='" + title + '\'' + ", author='" + author + '\'' + '}';
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Book book = (Book) o;
-        return Objects.equals(title, book.title) && Objects.equals(author, book.author);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || getClass() != o.getClass()) return false;
+    Book book = (Book) o;
+    return Objects.equals(title, book.title) && Objects.equals(author, book.author);
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(title, author);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(title, author);
+  }
 }

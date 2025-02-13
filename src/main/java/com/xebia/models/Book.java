fdd5b894b1e3932d8204794d64e6c6f823e9e0bd
Flagn3 <flagn3@gmail.com>
2025-02-13 09:29:25 +0100

    updated Book model

--- src/main/java/com/xebia/models/Book.java
+++ src/main/java/com/xebia/models/Book.java
@@ -1,11 +1,13 @@
 package com.xebia.models;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 public class Book {
 
   private String title;
   private String author;
+  private LocalDate date;
 
   public Book(String title, String author) {
     this.title = title;
@@ -20,20 +22,35 @@ public class Book {
     return author;
   }
 
+  public LocalDate getDate() {
+    return date;
+  }
+
   @Override
   public String toString() {
-    return "Book{" + "title='" + title + '\'' + ", author='" + author + '\'' + '}';
+    return "Book{"
+        + "title='"
+        + title
+        + '\''
+        + ", author='"
+        + author
+        + '\''
+        + ", date="
+        + date
+        + '}';
   }
 
   @Override
   public boolean equals(Object o) {
     if (o == null || getClass() != o.getClass()) return false;
     Book book = (Book) o;
-    return Objects.equals(title, book.title) && Objects.equals(author, book.author);
+    return Objects.equals(title, book.title)
+        && Objects.equals(author, book.author)
+        && Objects.equals(date, book.date);
   }
 
   @Override
   public int hashCode() {
-    return Objects.hash(title, author);
+    return Objects.hash(title, author, date);
   }
 }

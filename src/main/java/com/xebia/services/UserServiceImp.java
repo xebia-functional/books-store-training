47b91ef4e1af253f36d4f5be7820cc58f0c24e55
AgcioVF <alberto.viserasf@gmail.com>
2025-02-13 12:56:43 +0100

    Create UserServiceImp

---
+++ src/main/java/com/xebia/services/UserServiceImp.java
@@ -0,0 +1,65 @@
+package com.xebia.services;
+
+import com.xebia.models.User;
+import java.util.*;
+import java.util.logging.Logger;
+
+public class UserServiceImp implements UserService {
+
+  private List<User> users;
+  private Logger logger;
+
+  public UserServiceImp(Logger logger) {
+    this.users = new ArrayList<>();
+    this.logger = logger;
+  }
+
+  @Override
+  public boolean addUser(User newUser) {
+    for (User u : users) {
+      if (users.contains(newUser)) {
+        logger.warning("The user already exists.");
+        return false;
+      }
+    }
+    this.users.add(newUser);
+    return true;
+  }
+
+  @Override
+  public boolean removeUser(User remUser) {
+    for (User u : users) {
+      if (users.equals(remUser)) {
+        users.remove(remUser);
+        logger.info("The user has been removed successfully.");
+        return true;
+      }
+    }
+    logger.warning("The user: " + remUser.getName() + " doesn't exist.");
+    return false;
+  }
+
+  @Override
+  public Optional<User> searchUser(String userName) {
+    for (User u : users) {
+      if (u.getName().equalsIgnoreCase(userName)) {
+        return Optional.of(u);
+      }
+    }
+    logger.warning("No user register under the name " + userName);
+    return Optional.empty();
+  }
+
+  @Override
+  public List<User> listUsers() {
+    logger.info("----- User's list -----");
+    if (users.isEmpty()) {
+      logger.info("No users registered.");
+    } else {
+      for (User u : users) {
+        logger.info(u.toString());
+      }
+    }
+    return users;
+  }
+}

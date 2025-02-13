ea5959320c89910e705bdcf038701d8252e2dc7d
AgcioVF <alberto.viserasf@gmail.com>
2025-02-13 11:26:24 +0100

    Update UserService

--- src/main/java/com/xebia/services/UserService.java
+++ src/main/java/com/xebia/services/UserService.java
@@ -4,12 +4,34 @@ import com.xebia.models.User;
 import java.util.*;
 
 public interface UserService {
-
+  /**
+   * This method adds a ser to the User list
+   *
+   * @param newUser
+   * @return boolean
+   */
   boolean addUser(User newUser);
 
+  /**
+   * This method adds a ser to the User list
+   *
+   * @param remUser
+   * @return boolean
+   */
   boolean removeUser(User remUser);
 
-  Optional<User> searchUser(String Username);
+  /**
+   * This method returns the user that matches with the parameter userName
+   *
+   * @param userName
+   * @return Optional.of(user)
+   */
+  Optional<User> searchUser(String userName);
 
+  /**
+   * This method returns a list of the registered users
+   *
+   * @return List<User>
+   */
   List<User> listUsers();
 }

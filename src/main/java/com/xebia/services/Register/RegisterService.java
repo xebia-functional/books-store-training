3847bf3d028e2a6ac0235570ed3b8b372a028142
AgcioVF <alberto.viserasf@gmail.com>
2025-02-18 11:29:28 +0100

    Update RegisterService.java

--- src/main/java/com/xebia/services/Register/RegisterService.java
+++ src/main/java/com/xebia/services/Register/RegisterService.java
@@ -1,3 +1,51 @@
 package com.xebia.services.Register;
 
-public interface RegisterService {}
+import com.xebia.models.Register;
+import java.time.LocalDate;
+import java.util.*;
+
+public interface RegisterService {
+
+  /**
+   * Method that adds a new register
+   *
+   * @param userID
+   * @param bookID
+   * @param rentDate
+   * @return
+   */
+  boolean addRegister(UUID userID, UUID bookID, LocalDate rentDate);
+
+  /**
+   * Method that removes a register
+   *
+   * @param userID
+   * @param bookID
+   * @param rentDate
+   * @return
+   */
+  boolean removeRegister(UUID userID, UUID bookID, LocalDate rentDate);
+
+  /**
+   * Method ta searches a register by userID
+   *
+   * @param userID
+   * @return
+   */
+  boolean searchRegisterByUser(UUID userID);
+
+  /**
+   * Method ta searches a register by BookID
+   *
+   * @param BookID
+   * @return
+   */
+  boolean searchRegisterByBook(UUID BookID);
+
+  /**
+   * Method that lists all registered elements
+   *
+   * @return
+   */
+  List<Register> listRegistered();
+}

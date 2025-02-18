cd85b37193be90340dbbfe738a52b778063b7b7e
AgcioVF <alberto.viserasf@gmail.com>
2025-02-18 13:30:23 +0100

    Update TestRegisterServiceSuite.java

--- src/test/java/com/xebia/TestRegisterServiceSuite.java
+++ src/test/java/com/xebia/TestRegisterServiceSuite.java
@@ -1,153 +1,153 @@
 package com.xebia;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import com.xebia.models.Register;
 import com.xebia.services.Register.RegisterService;
 import com.xebia.services.Register.RegisterServiceImp;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 import java.util.logging.Logger;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestRegisterServiceSuite {
 
-    private List<Register> registers;
-    private Logger logger;
-    private RegisterService registerService;
+  private List<Register> registers;
+  private Logger logger;
+  private RegisterService registerService;
 
-    @BeforeEach
-    void setUp() {
-        logger = Logger.getLogger(TestUserServiceSuite.class.getName());
-        registerService = new RegisterServiceImp(logger);
-    }
+  @BeforeEach
+  void setUp() {
+    logger = Logger.getLogger(TestUserServiceSuite.class.getName());
+    registerService = new RegisterServiceImp(logger);
+  }
 
-    @Test
-    public void addRegisterShouldWork(){
-        // Given
-        // When
-        boolean result = registerService.addRegister(UUID.randomUUID(),UUID.randomUUID(), LocalDate.now());
+  @Test
+  public void addRegisterShouldWork() {
+    // Given
+    // When
+    boolean result =
+        registerService.addRegister(UUID.randomUUID(), UUID.randomUUID(), LocalDate.now());
 
-        // Expected
-        assertTrue(result);
-    }
+    // Expected
+    assertTrue(result);
+  }
 
-    @Test
-    public  void  addRegisterShouldWorkWhenDuplicate(){
-        // Given
-        UUID duplicateBook = UUID.randomUUID();
-        UUID duplicateUser = UUID.randomUUID();
-        registerService.addRegister(duplicateUser,duplicateBook, LocalDate.now());
+  @Test
+  public void addRegisterShouldWorkWhenDuplicate() {
+    // Given
+    UUID duplicateBook = UUID.randomUUID();
+    UUID duplicateUser = UUID.randomUUID();
+    registerService.addRegister(duplicateUser, duplicateBook, LocalDate.now());
 
-        //When
-        boolean result = registerService.addRegister(duplicateUser,duplicateBook, LocalDate.now());
+    // When
+    boolean result = registerService.addRegister(duplicateUser, duplicateBook, LocalDate.now());
 
-        // Expected
-        assertFalse(result);
-    }
+    // Expected
+    assertFalse(result);
+  }
 
-    @Test
-    public  void  removeRegisterShouldWork(){
-        // Given
-        UUID sampleBook = UUID.randomUUID();
-        UUID sampleUser = UUID.randomUUID();
-        registerService.addRegister(sampleUser, sampleBook, LocalDate.now());
+  @Test
+  public void removeRegisterShouldWork() {
+    // Given
+    UUID sampleBook = UUID.randomUUID();
+    UUID sampleUser = UUID.randomUUID();
+    registerService.addRegister(sampleUser, sampleBook, LocalDate.now());
 
-        //When
-        boolean result = registerService.removeRegister(sampleUser,sampleBook, LocalDate.now());
+    // When
+    boolean result = registerService.removeRegister(sampleUser, sampleBook, LocalDate.now());
 
-        // Expected
-        assertTrue(result);
-    }
+    // Expected
+    assertTrue(result);
+  }
 
-    @Test
-    public  void  removeRegisterShouldWorkWhenNotFound(){
-        // Given
-        //When
-        boolean result = registerService.removeRegister(UUID.randomUUID(),UUID.randomUUID(), LocalDate.now());
+  @Test
+  public void removeRegisterShouldWorkWhenNotFound() {
+    // Given
+    // When
+    boolean result =
+        registerService.removeRegister(UUID.randomUUID(), UUID.randomUUID(), LocalDate.now());
 
+    // Expected
+    assertFalse(result);
+  }
 
-        // Expected
-        assertFalse(result);
-    }
+  @Test
+  public void searchRegisterByUserShouldWork() {
+    // Given
+    UUID sampleUser = UUID.randomUUID();
+    registerService.addRegister(sampleUser, UUID.randomUUID(), LocalDate.now());
 
-    @Test
-    public void searchRegisterByUserShouldWork(){
-        // Given
-        UUID sampleUser = UUID.randomUUID();
-        registerService.addRegister(sampleUser, UUID.randomUUID(), LocalDate.now());
+    // When
+    List<Register> registerSearch = registerService.searchRegisterByUser(sampleUser);
 
-        // When
-        List<Register> registerSearch = registerService.searchRegisterByUser(sampleUser);
+    // Expected
+    assertFalse(registerSearch.isEmpty());
+  }
 
-        // Expected
-        assertFalse(registerSearch.isEmpty());
-    }
+  @Test
+  public void searchRegisterByUserShouldWorkWhenNotFound() {
+    // Given
+    UUID sampleUser = UUID.randomUUID();
+    registerService.addRegister(sampleUser, UUID.randomUUID(), LocalDate.now());
 
-    @Test
-    public void searchRegisterByUserShouldWorkWhenNotFound(){
-        // Given
-        UUID sampleUser = UUID.randomUUID();
-        registerService.addRegister(sampleUser, UUID.randomUUID(), LocalDate.now());
+    // When
+    List<Register> registerSearch = registerService.searchRegisterByUser(UUID.randomUUID());
 
-        // When
-        List<Register> registerSearch = registerService.searchRegisterByUser(UUID.randomUUID());
+    // Expected
+    assertTrue(registerSearch.isEmpty());
+  }
 
-        // Expected
-        assertTrue(registerSearch.isEmpty());
-    }
+  @Test
+  public void searchRegisterByBookShouldWork() {
+    // Given
+    UUID sampleBook = UUID.randomUUID();
+    registerService.addRegister(UUID.randomUUID(), sampleBook, LocalDate.now());
 
-    @Test
-    public void searchRegisterByBookShouldWork(){
-        // Given
-        UUID sampleBook = UUID.randomUUID();
-        registerService.addRegister(UUID.randomUUID(), sampleBook, LocalDate.now());
+    // When
+    List<Register> registerSearch = registerService.searchRegisterByBook(sampleBook);
 
-        // When
-        List<Register> registerSearch = registerService.searchRegisterByBook(sampleBook);
+    // Expected
+    assertFalse(registerSearch.isEmpty());
+  }
 
-        // Expected
-        assertFalse(registerSearch.isEmpty());
-    }
+  @Test
+  public void searchRegisterByBookShouldWorkWhenNotFound() {
+    // Given
+    UUID sampleBook = UUID.randomUUID();
+    registerService.addRegister(UUID.randomUUID(), sampleBook, LocalDate.now());
 
-    @Test
-    public void searchRegisterByBookShouldWorkWhenNotFound(){
-        // Given
-        UUID sampleBook = UUID.randomUUID();
-        registerService.addRegister(UUID.randomUUID(), sampleBook, LocalDate.now());
+    // When
+    List<Register> registerSearch = registerService.searchRegisterByBook(UUID.randomUUID());
 
-        // When
-        List<Register> registerSearch = registerService.searchRegisterByBook(UUID.randomUUID());
+    // Expected
+    assertTrue(registerSearch.isEmpty());
+  }
 
-        // Expected
-        assertTrue(registerSearch.isEmpty());
-    }
+  @Test
+  public void listRegisteredShouldWork() {
+    // Given
+    registerService.addRegister(UUID.randomUUID(), UUID.randomUUID(), LocalDate.now());
+    registerService.addRegister(UUID.randomUUID(), UUID.randomUUID(), LocalDate.now());
+    registerService.addRegister(UUID.randomUUID(), UUID.randomUUID(), LocalDate.now());
 
-    @Test
-    public void listRegisteredShouldWork(){
-        // Given
-        registerService.addRegister(UUID.randomUUID(),UUID.randomUUID(),LocalDate.now());
-        registerService.addRegister(UUID.randomUUID(),UUID.randomUUID(),LocalDate.now());
-        registerService.addRegister(UUID.randomUUID(),UUID.randomUUID(),LocalDate.now());
+    // When
+    registerService.listRegistered();
 
-        //When
-        registerService.listRegistered();
+    // Expected
+    assertEquals(3, registerService.listRegistered().size());
+  }
 
-        // Expected
-        assertEquals(3, registerService.listRegistered().size());
-    }
+  @Test
+  public void listRegisteredShouldWorkWhenEmpty() {
+    // Given
 
-    @Test
-    public void listRegisteredShouldWorkWhenEmpty(){
-        // Given
+    // When
+    registerService.listRegistered();
 
-        //When
-        registerService.listRegistered();
-
-        // Expected
-        assertEquals(0, registerService.listRegistered().size());
-    }
+    // Expected
+    assertEquals(0, registerService.listRegistered().size());
+  }
 }

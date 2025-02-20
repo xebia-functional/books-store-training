de2174ed13a7dc294ec79730fda44d70932ad306
Flagn3 <flagn3@gmail.com>
2025-02-20 14:43:17 +0100

    Update flyway

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("java")
     alias(libs.plugins.spotless)
-    id("org.flywaydb.flyway") version "9.5.0"
 }
 
 group = "com.xebia"
@@ -14,11 +13,8 @@ repositories {
 dependencies {
     testImplementation(platform(libs.junit.jupiter.bom))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("org.flywaydb:flyway-core:11.3.2")
+    implementation(libs.flyway)
     implementation("org.postgresql:postgresql:42.5.0")
-
-
-
 }
 
 tasks.test {
